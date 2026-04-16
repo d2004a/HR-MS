7437a06db6095119020223b4e9e428b145ca9d4a
Divyanshu agarwal <agarwaldivyanshu824@gmail.com>
2026-04-16 11:11:14 +0530

    final cors issue

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,12 @@ connectDB();
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://hr-ms-frontend-ten.vercel.app"
-];
+
 
 // Middleware
+
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS not allowed"));
-    }
-  },
+  origin: "https://hr-ms-frontend-ten.vercel.app",
   credentials: true
 }));
 
